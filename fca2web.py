ef43966f820ed47b5834063906ede10877a5b0aa
Fabiano Castello <fabianocastello@gmail.com>
2021-07-31 23:08:13 -0300

    improovements

--- fca2web.py
+++ fca2web.py
@@ -11,18 +11,37 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from glob import glob
 import textwrap
-
+import socket
 from PIL import Image
-
-# setup Streamlit
+from random import randrange
 import streamlit as st
 from streamlit import caching
+
+#global UserAgents, runningOn, parâmetros, remarks, sizeKB
+runningOn =  socket.gethostname()
+
+#USerAgents para post das estatísticas
+with open("./UserAgents.cfg",encoding='utf-8') as f:
+     UserAgents = f.readlines(); f.close()
+UserAgents = [c.replace('\n','').strip() for c in UserAgents]
+
+if not runningOn == 'localhost':
+    import configparser
+    config_parser = configparser.RawConfigParser()
+    config_parser.read('./fca2web.ini')
+    url_post_stat  = config_parser.get('RUN', 'url_post_stat')
+else:
+    pass #colocar o secrets
+
 caching.clear_cache()
 st.set_page_config(
         page_title="FCA2 - FCastell Auto Analyser")
         
 def run():
-    global msg_count, max_freq, hist_bins, max_dups, hist, mcorr
+    global msg_count, max_freq, hist_bins, max_dups
+    global hist, boxp, mcorr, noheader, sep_csv
+    global UserAgents, runningOn, parâmetros, remarks, sizeKB
+
     st.markdown(f'''
     <body>
     <p style="font-size:50px;line-height: 25px"><i><b>FCA2</b></i><br>
@@ -45,12 +64,19 @@ def run():
     with col3:
         max_dups = st.slider('qte de exemplos de duplicados:', 1, 10, max_dups)
         
-    col1, col2 = st.beta_columns(2)
+    col1, col2, col3 = st.beta_columns(3)
     with col1:
-        hist  =  st.checkbox('Mostrar histogramas?', value=False)
+        hist  =  st.checkbox('Mostrar histograma?', value=False)
     with col2:
+        boxp  =  st.checkbox('Mostrar boxplot?', value=False)
+    with col3:
         mcorr =  st.checkbox('Mostrar matriz de correlação?', value=False)
 
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        noheader  =  st.checkbox('Marque se o seu arquivo não tem header (cabeçalho)', value=False)
+    with col2:
+        sep_csv  =  st.text_input('Para arquivos CSV, quer definir um separador específico?', value='')
  
     #print(max_freq, hist_bins, max_dups, hist)
  
@@ -58,60 +84,73 @@ def run():
     try:    del uploaded_files
     except: pass
         
-    uploaded_files = st.file_uploader("Arquivo para análise:", type =['csv','xls','xlsx'],
-                    accept_multiple_files=True)
+    uploaded_file = st.file_uploader("Arquivo para análise:", type =['csv','xls','xlsx'],
+                    accept_multiple_files=False)
     # UploadedFile(id=8, name='xxxxjr5mqz0i.csv', type='application/vnd.ms-excel', size=1211)
     
-    
-    for uploaded_file in uploaded_files:
-        if uploaded_file is not None:
-            with open(os.path.join(datain,uploaded_file.name),"wb") as f:
-                f.write(uploaded_file.getbuffer())
+    # ago/21 - restrito para um arquivo apenas
+    if uploaded_file is not None:
+        with open(os.path.join(datain,uploaded_file.name),"wb") as f:
+            f.write(uploaded_file.getbuffer())
     files = os.listdir(datain)
     if len(files) != 0:
         dfx = [i for i in files if ('XLSX' in i[-4:].upper()) or ('XLS' in i[-3:].upper())]
         dfc = [i for i in files if 'CSV' in i[-3:].upper()] 
         filesToWorkOn = dfx + dfc
-        msg_count = 0
-        st.markdown(f'''<body><p style="font-size:14px;margin-bottom: -5px;
-                    font-family: Courier;">{msgC()} analisando {len(filesToWorkOn)} 
-                    arquivo{"(s)" if len(filesToWorkOn) > 1 else ""}.
-                    </p></body>''', unsafe_allow_html=True)
-
-        
+        msg_count  = 0
         
         del dfx, dfc, files
         filesToWorkOn = sorted(filesToWorkOn) 
         for name in filesToWorkOn:
             st.markdown(f'''<body><p style="font-size:20px;margin-bottom: -5px;">
                             Analisando <b>{name}</b></p></body>''', unsafe_allow_html=True)
+                            
+            parâmetros = ''
+            remarks = ''
+            sizeKB     = round(os.stat(name).st_size/1024,2)
+                            
             analysis(name)
             
 
         st.markdown(f'''<body><p style="font-size:15px;margin-bottom: -5px;"><br><br><br>
-                            <u><i>✅ Para analisar outros arquivos pressione F5</i></u></p></body>''', unsafe_allow_html=True)
+                            <u><i>✅ Para analisar outro arquivo pressione F5</i></u></p></body>''', unsafe_allow_html=True)
     
     st.markdown('''
     <body>
     <p style="font-size:30px;margin-bottom: -5px;">
-    <br><br><br><br>
+    <br>
     <b>Sobre FCA2</b></p>
     <p style="font-size:16px;margin-bottom: -5px;">
-    FCA2 é um algoritmo criado originalmente em Python para análise exploratória básica de dados, que visa trazer <b>produtividade</b> para analistas. De forma automática, o algoritmo trata arquivos em formato csv, xls e xlsx e realiza diversas análises: <span style="font-size:10px ;margin-bottom: -5px;">
+    FCA2 é um algoritmo criado em Python para análise exploratória básica de dados, que visa trazer <b>produtividade</b> para analistas. De forma automática, o algoritmo trata arquivos em formato csv, xls e xlsx e realiza diversas análises: <span style="font-size:10px ;margin-bottom: -5px;"> 
     <ul style="margin-bottom: -5px;">
       <li>identificação de campos texto, campos numéricos inteiros e números decimais.</li>
       <li>campos texto: quantidade de registros, duplicações e de categorias, top "n" categorias.</li>
       <li>campos numéricos: quantidade de registros, registros zerados, soma total, média, desvio, máximos e mínimos, amplitude, quartis. Mesmas descrições para a base descontando os registros zerados.</li>
-    </ul></span><span style="font-size:16px ;line-height: 25px">
-    Desenvolvido originalmente por Fabiano Castello (<a target="_blank" href ="http://www.fabianocastello.com.br">www.fabianocastello.com.br</a>), é disponibilizado sob licença GNL3.0 para toda a comunidade. A versão web foi criada em streamlit (<a target="_blank" href ="http://www.github.com/fabianocastello/fca2web">www.github.com/fabianocastello/fca2web</a>), e o código original em Python também está disponível (<a target="_blank" href ="http://www.github.com/fabianocastello/fca2">www.github.com/fabianocastello/fca2</a>). FCA2 é disponibilizado em beta: use por seu próprio risco. O código original está registrado sob DOI <a target="_blank" href ="http://doi.org/10.6084/m9.figshare.9902417">doi.org/10.6084/m9.figshare.9902417</a>.</span></p><br>
+    </ul></span><span style="font-size:16px ;line-height: 25px"><br>
+    Desenvolvido originalmente por Fabiano Castello (<a target="_blank" href ="http://www.fabianocastello.com.br">www.fabianocastello.com.br</a>), é disponibilizado sob licença CC BY 4.0 para toda a comunidade. O código original está registrado sob DOI <a target="_blank" href ="http://doi.org/10.6084/m9.figshare.9902417">doi.org/10.6084/m9.figshare.9902417</a>. A versão web foi criada em streamlit e está disponível em (<a target="_blank" href ="http://www.github.com/fabianocastello/fca2web">www.github.com/fabianocastello/fca2web</a>). Se você usar esta aplicação em um artigo ou publicação pode incluir a citação "Castello, Fabiano (2019): <i>Python Code: FC Auto Analyser (FCA2)</i>. figshare. Software. https://doi.org/10.6084/m9.figshare.9902417.v1".</span> </p><br>
+    
+    <p style="font-size:20px;margin-bottom: -5px;"><b><i>What's New</i></b></p>
+    <p style="font-size:16px;margin-bottom: -5px<span style="font-size:10px ;margin-bottom: -5px;"> 
+    <ul style="margin-bottom: -5px;">
+      <li>mudança do licenciamento para CC BY 4.0 (jul/21).</li>
+      <li>boxplot (ago/21).</li>
+    </ul></span><span style="font-size:16px ;line-height: 25px"><br>
+   
+    <p style="font-size:20px;margin-bottom: -5px;"><b><i>Issues</i> conhecidos</b></p>
+    <p style="font-size:16px;margin-bottom: -5px;">
+    Neste momento o único "issue" conhecido é a questão do alinhamento dos resultados no browser, por um problema de fontes de HTML nos navegadores, particulamente referente aos "white spaces". </p><br>
+    
+    <p style="font-size:20px;margin-bottom: -5px;"><b>O que está no <i>pipeline</i></b></p>
+    <p style="font-size:16px;margin-bottom: -5px<span style="font-size:10px ;margin-bottom: -5px;"> 
+    <ul style="margin-bottom: -5px;">
+      <li>gerar um arquivo em PDF com a análise consolidada.</li>
+      <li>mudar o resultado da análise para uma tabela como solução para a questão da formatação no browser.</li>
+    </ul></span><span style="font-size:16px ;line-height: 25px"><br>
+    
     <p style="font-size:20px;margin-bottom: -5px;"><b>Sobre LGPD, GRPR e confidencialidade de dados</b></p>
     <p style="font-size:16px;margin-bottom: -5px;">
-    FCA2 cria containers a partir dos arquivos carregados para tratamento e destrói a informação assim que o processamento é realizado. Nenhuma informação é retida ou enviada para fora do site. Todos os arquivos tempororários geradaos são apagados.</p><br>
-   
-    <p style="font-size:20px;margin-bottom: -5px;"><b>Problemas & Melhorias</b></p>
-    <p style="font-size:16px;margin-bottom: -5px;">
-    Vamos trabalhar para melhorar cada vez mais o aplicativo. Neste momento o único "issue" conhecido é a questão do alinhamento dos resultados no browser, por um problema de fontes de HTML nos navegadores, particulamente referente aos "white spaces". </p><br>
-    
+    FCA2 cria containers a partir dos arquivos carregados para tratamento e destrói a informação assim que o processamento é realizado. Nenhuma informação é retida ou enviada para fora do site (exceto tipo do arquivo, tamanho e tempo de processamento, para efeito de gerar estatísticas). Todos os arquivos tempororários geradaos são apagados.</p><br>
+
     <p style="font-size:20px;margin-bottom: -5px;"><b>Contribuições</b></p>
     <p style="font-size:16px;margin-bottom: -5px;">
     FCA2 é mantido pelo autor com contribuições da comunidade.<br>Thanks: Marcus Pinto, João Victor Mulle, Mateus Ricci, Vivian Sato. </p><br>
@@ -137,7 +176,7 @@ max_freq  = 10 #numeros de categorias máximas nos campos texto
 hist_bins = 10 #qte de bins no histograma 
 max_dups  = 4  #qte de exemplos de duplicados 
 
-FC_Auto_Analyser_Version = 'fca2web beta 0.91 (jul/21) '
+FC_Auto_Analyser_Version = 'fca2web beta 0.92 (2021AGO01) '
 
 #Criando diretórios se inexistentes
 dirs = ['!data.tmp','!data.out','!data.log','!data.in']
@@ -147,6 +186,19 @@ for d in dirs:
     else:
         files = glob(f'{d}/*')
         for f in files: os.remove(f)
+        
+def post_stat_fca2(filename, sizeKB, parâmetros, elap,remarks=''):
+    
+    form_data = { 'entry.525617926' : runningOn,
+                  'entry.506775480' : filename,
+                  'entry.442023490' : sizeKB,
+                  'entry.2027178672': elap,
+                  'entry.222084486' : parâmetros,
+                  'entry.225285568' : runningOn,
+                  'entry.1052496244': remarks }
+    return(requests.post(url_post_stat+'/formResponse', data=form_data, headers=
+                 {'Referer':url_post_stat+'/viewform',
+                  'User-Agent': UserAgents[randrange(len(UserAgents))]}))
 
 global msg_count
 msg_count = 0
@@ -186,31 +238,47 @@ def sep(ser):
 def analysis(file):
     
     global df,ctmp, ctmp_counts, i, x, xqte_corr  #apenas para teste, retirar na versão final
-
+    global start_time
     if True: #try:
         log_write("Iniciando análise de "+file)
-        
+        start_time = time.time()
+
         # open the file
         if 'csv' in file:
             try:
-                f = open(datain+"/"+file,encoding='utf-8')
-                line = f.readline()
-                f.close()
-                semicolon = line.count(";")
-                comma = line.count(",")
-                if semicolon > comma:
-                    separador = ";"
+                if len(sep_csv.strip()) == 0:
+                    f = open(datain+"/"+file,encoding='utf-8')
+                    line = f.readline()
+                    f.close()
+                    semicolon = line.count(";")
+                    comma = line.count(",")
+                    if semicolon > comma:
+                        separador = ";"
+                    else:
+                        separador = ","
                 else:
-                    separador = ","
+                    separador = sep_csv.strip()
                 log_write("Separador de CSV selecionado [ "+separador+" ]")
-                df = pd.read_csv(datain+"/"+file, encoding ='utf-8', engine='python', sep = separador)
+                if noheader:
+                    df = pd.read_csv(datain+"/"+file, encoding ='utf-8', engine='python', sep = separador, header=None)
+                    for c in df.columns:
+                        df.rename({c:f'Coluna{c}'}, axis=1, inplace=True)
+                       
+                else:
+                    df = pd.read_csv(datain+"/"+file, encoding ='utf-8', engine='python', sep = separador)
             except Exception as erro:
                 log_write("Erro "+str(erro))
                 log_write("Abortando analise "+file+"\n")
                 return(-1)
         elif 'xls'in file:
             try:
-                df = pd.read_excel(datain+"/"+file)
+                if noheader:
+                    df = pd.read_excel(datain+"/"+file, header=None)
+                    for c in df.columns:
+                        df.rename({c:f'Coluna{c}'}, axis=1, inplace=True)
+                else:
+                    df = pd.read_excel(datain+"/"+file)
+                
             except Exception as erro:
                 log_write("Erro "+str(erro))
                 log_write("Abortando analise "+file+"\n")
@@ -218,6 +286,13 @@ def analysis(file):
         else:
             log_write("Erro identificando xls/csv")
             return(-1)
+            
+        analysis_df(df,file)
+        return(None)
+        
+        
+def analysis_df(df,file):
+         
         
         ## MORFOLOGIA
         reg_total = df.shape[0]
@@ -314,11 +389,11 @@ def analysis(file):
                     cont = 0
                     dupsTX = '['
                     for index, row in dupsTMP.iterrows():
-                        dupsTX += f'{row[x].strip()}({row[0]}), '
+                        dupsTX += f'{str(row[x]).strip()}({row[0]}), '
                         cont += 1
                         if cont >= max_dups: break
                     dupsTX = dupsTX.strip()[:-1]+']'
-                    log_write(f"Duplicações (Eventos):", addcont=False, newline=True) 
+                    log_write(f"Duplicações ({cont} exemplos):", addcont=False, newline=True) 
                     log_write(f"{dupsTX}", addcont=False) 
                                              
                             
@@ -419,7 +494,36 @@ def analysis(file):
                         log_write('', addcont=False,newline=True)  
 
                         image = Image.open(dataout+"/"+img_file)
-                        st.image(image, caption=img_file)                        
+                        st.image(image, caption=img_file) 
+						
+						
+                    if boxp:
+                        grf = df[x].dropna()    
+                        grfINFO = grf.describe()
+                        max_height = (np.histogram(grf, bins=hist_bins)[0].max())
+                        max_lenght = (grfINFO[7]- grfINFO[3])
+                        sns.set_style("whitegrid")
+
+                        plt.figure(figsize=(11.7, 8.27))
+                        plt.xlim(grfINFO[3].round(0), grfINFO[7].round(0))
+                        plt.ylim(0, max_height*1.1)
+                        
+                        plt.text(0.03*max_lenght,-0.09*max_height,
+                                 FC_Auto_Analyser_Version+'\n'+'http://github.com/fabianocastello/fca2web',
+                                 fontsize=10, ha='left')
+                        
+                        sns_plot = sns.boxplot(x=grf)
+                        sns_plot = sns_plot.set_title("Boxplot de ["+grf.name+"]", {'size': '18'})
+                        img_file = file+'_BoxPlot_'+grf.name+'.png'
+                        fig = sns_plot.get_figure().savefig(dataout+"/"+img_file)  
+                        plt.close()
+                        log_write('', addcont=False,newline=True)  
+
+                        image = Image.open(dataout+"/"+img_file)
+                        st.image(image, caption=img_file) 
+
+
+						
                         
 
         ## CORRELAÇÃO ENTRE VARIÁVEIS NUMÉRICAS
@@ -441,10 +545,23 @@ def analysis(file):
             else:
                 log_write('Não foram identificadas variáveis numéricas')
 
-            log_write(file+" ending. Bye!\n")
+           
+       
+        running_time = str(int(round( (time.time() - start_time)/60,0)) )+'min'
+       
+        log_write(f"Running time: {running_time}", newline=True,addcont=True) 
+        
+        #post statísticas
+        ret = post_stat_fca2(file, sizeKB, parâmetros, running_time, remarks)
+        if '200' in str(ret):
+            log_write(f"Estatística postada: nome={file}, size={sizeKB:,}Kb, param={parâmetros}"+\
+                      f", Elap={running_time}, remarks={remarks}",
+                        newline=False,addcont=True) 
+        log_write("Análise finalizada de "+file, newline=False) 
+
             
-            log_write("Análise finalizada de "+file,newline=True) 
-            return(0)
+            
+        return(0)
 
     #except Exception as erro:
     #    log_write("\n\n Erro Geral: "+str(erro) + "\n\n") 
